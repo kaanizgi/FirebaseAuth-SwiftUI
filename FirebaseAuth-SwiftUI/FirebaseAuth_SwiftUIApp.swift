7e824aba75a1e9269732b86478b836ab0fa0e63a
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-30 10:26:09 +0300

    Initial Commit

---
+++ FirebaseAuth-SwiftUI/FirebaseAuth_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuth_SwiftUIApp.swift
+//  FirebaseAuth-SwiftUI
+//
+//  Created by Kaan İzgi on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuth_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
